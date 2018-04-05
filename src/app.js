bfffb22c1b41e1b174aae89eaa91b2e1925e7672
karol <karol.kowalski95@gmail.com>
2018-04-05 12:15:59 +0200

    Added header

--- src/app.js
+++ src/app.js
@@ -1,27 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './playground/myBoard';
+import Header from './components/Header';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const data = {
-    lanes: [
-      {
-        id: 'lane1',
-        title: 'Planned Tasks',
-        label: '2/2',
-        cards: [
-          {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins'},
-          {id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
-        ]
-      },
-      {
-        id: 'lane2',
-        title: 'Completed',
-        label: '0/0',
-        cards: []
-      }
-    ]
-  }
-
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<Header/>, document.getElementById('app'));
