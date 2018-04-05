c0e53ecaa2562e65f5b1f3ec5498981a0dc3bb1f
karol <karol.kowalski95@gmail.com>
2018-04-05 22:56:51 +0200

    Creating Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,70 @@
 import React from 'react';
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-const Card = ({id, title}) => (
-    <div className="card">
-        <div className="card-title">
-            {title}
-        </div>
-    </div>
-);
+class Card extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+          isModalOpen: false
+        };
+      }
 
-export default Card;
+      toggleCardEditor = () => {
+        this.setState({ isModalOpen: !this.state.isModalOpen });
+      };
+    
+      handleClick = event => {
+        const { tagName, checked, id } = event.target;
+        if (tagName.toLowerCase() === "input") {
+          // The id is a string that describes which number in the order of checkboxes this particular checkbox has
+          this.toggleCheckbox(checked, parseInt(id, 10));
+        } else if (tagName.toLowerCase() !== "a") {
+          this.toggleCardEditor(event);
+        }
+      };
+    
+      handleKeyDown = event => {
+        // Only open card on enter since spacebar is used by react-beautiful-dnd for keyboard dragging
+        if (event.keyCode === 13 && event.target.tagName.toLowerCase() !== "a") {
+          event.preventDefault();
+          this.toggleCardEditor();
+        }
+      };
+    
+      // identify the clicked checkbox by its index and give it a new checked attribute
+      toggleCheckbox = (checked, i) => {
+        const { card, dispatch } = this.props;
+    
+        let j = 0;
+        const newText = card.text.replace(/\[(\s|x)\]/g, match => {
+          let newString;
+          if (i === j) {
+            newString = checked ? "[x]" : "[ ]";
+          } else {
+            newString = match;
+          }
+          j += 1;
+          return newString;
+        });
+    
+        dispatch({
+          type: "CHANGE_CARD_TEXT",
+          payload: { cardId: card._id, cardText: newText }
+        });
+      };
+}
+
+Card.propTypes = {
+    card: PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        text: PropTypes.string.isRequired,
+        color: PropTypes.string
+      }).isRequired,
+      listId: PropTypes.string.isRequired,
+      isDraggingOver: PropTypes.bool.isRequired,
+      index: PropTypes.number.isRequired,
+      dispatch: PropTypes.func.isRequired
+};
+
+export default connect(mapStateToProps)(Card);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import FaUserSecret from "react-icons/lib/fa/user-secret";
+import FaSignOut from "react-icons/lib/fa/sign-out";
+import FaSignIn from "react-icons/lib/fa/sign-in";
+import kanbanLogo from "../../../assets/images/kanban-logo.svg";
 
-const Header = (props) => (
-    <div className="header">
-    <div className="container">
-      <h1 className="header__title">{props.title}</h1>
-      {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
-    </div>
-  </div>
-)
+class Header extends Component {
+  static propTypes = { user: PropTypes.object };
+  render = () => {
+    const { user } = this.props;
+    return (
+      <header>
+        <Link to="/" className="header-title">
+          <img src={kanbanLogo} alt="React Kanban logo" />
+          &nbsp;React Kanban
+        </Link>
+        <div className="header-right-side">
+          {user ? (
+            <img
+              src={user.imageUrl}
+              alt={user.name}
+              className="user-thumbnail"
+              title={user.name}
+            />
+          ) : (
+            <FaUserSecret className="guest-icon" />
+          )}
+          {user ? (
+            <a className="signout-link" href="/auth/signout">
+              <FaSignOut className="signout-icon" />
+              &nbsp;Sign out
+            </a>
+          ) : (
+            <a className="signout-link" href="/">
+              <FaSignIn className="signout-icon" />
+              &nbsp;Sign in
+            </a>
+          )}
+        </div>
+      </header>
+    );
+  };
+}
 
-Header.defaultProps = {
-    title: 'ReactoveEstGame',
-    subtitle: 'Reactive Estimation Game'
-};
+const mapStateToProps = ({ user }) => ({ user });
 
-export default Header;
+export default connect(mapStateToProps)(Header);
