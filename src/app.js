4b49cb4dfdab39a998f245815748b2a2541c9344
karol <karol.kowalski95@gmail.com>
2018-04-06 19:47:10 +0200

    Powrót do starej tablicy

--- src/app.js
+++ src/app.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './routers/AppRouter';
-import configureStore from './store/configureStore';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import App from './playground/myBoard';
+import './styles/styles.scss'
 
-const store = configureStore();
-
-const jsx = (
-    <Provider store={store}>
-        <AppRouter/>
-    </Provider>
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('app'));
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,9 +2,6 @@ import { createStore, combineReducers } from 'redux';
 import Cards from "../reducers/Cards";
 import Lists from "../reducers/List";
 import Boards from "../reducers/Board";
-import user from "./user";
-import isGuest from "./isGuest";
-import currentBoardId from "./currentBoardId";
 
 export default () => {
     const store = createStore(
